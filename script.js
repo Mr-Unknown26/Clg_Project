1bda8e801c22032af4f1dbf56e4881d62c7046ab
Afzan Arshad Shaikh <afzanmr374@gmail.com>
2025-05-03 23:08:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(('.skills'),{
+    strings:['Frontend Developer','Gamer','Student','Rider','Video Editer'],
+    typespeed : 100,
+    backspeed : 100,
+    backDelay : 1000,
+    loop : true,
+});
